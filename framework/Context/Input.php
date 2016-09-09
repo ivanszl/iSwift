dc7c777d34c255bb1376e3bb7134e6929014d5a2
IvanLam <ivan.lin.1985@gmail.com>
2016-09-09 23:47:26 +0800

    Update Input.php

--- framework/Context/Input.php
+++ framework/Context/Input.php
@@ -6,7 +6,8 @@ class Input extends \iSwift\Base\Application
     public function __construct()
     {
         $this->append([
-            'params' => []
+            'params' => [],
+            'server' => &$_SERVER
         ]);
     }
 
@@ -23,8 +24,33 @@ class Input extends \iSwift\Base\Application
         return isset($this->injectors['params'][$key]) ? $this->injectors['params'][$key] : $default;
     }
 
+    public function query($key, $default = null)
+    {
+        return $this->param($key, $default);
+    }
+    public function server($key, $default = null)
+    {
+        return isset($this->injectors['server'][$key]) ? $this->injectors['server'][$key] : $default;
+    }
+
     public function setParams($key, $value)
     {
         $this->injectors['params'][$key] = $value;
+    }
+
+    public function path()
+    {
+        if ($argv = $this->server('argv'))
+        {
+            if (substr($argv[1], 0, 2) != '--') {
+                return $argv[1];
+            }
+        }
+        return '/';
+    }
+
+    public function method()
+    {
+        return "GET";
     }
 }
